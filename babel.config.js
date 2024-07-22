ab27a128163e16f98a097376c07b182bbd5f7b97
taravannheng <hengtaravann@gmail.com>
2024-07-23 01:56:41 +0700

    feat(deps): add dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          
+        },
+      ],
+    ],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
