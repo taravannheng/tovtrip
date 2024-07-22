1364fc6a74316040c81676268a9bbf4924759176
taravannheng <hengtaravann@gmail.com>
2024-07-22 11:24:44 +0700

    feat(deps): add axios and react-native-paper dependencies

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>TovTrip</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
+    
   );
 }
 
